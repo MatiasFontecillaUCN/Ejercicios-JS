b83c4e0f805d12a41d7eed8cc1499da05c8f6f6a
MatiasFontecillaUCN <matias.fontecilla@alumnos.ucn.cl>
2023-11-07 15:08:56 -0300

    fix: 18.21 empty comment alert

--- 18.21/codigo.js
+++ 18.21/codigo.js
@@ -3,20 +3,23 @@ function validateField(id, value) {
   if (id === "registro_nombre" && value.trim() === "") {
     alert("Nombre es obligatorio");
     return false;
-  }
-  else if (id === "registro_apellido" && value.trim() === "") {
+  } else if (id === "registro_apellido" && value.trim() === "") {
     alert("Apellido es obligatorio");
     return false;
-  }
-  else if (id === "registro_email" && !emailValido(value)) {
+  } else if (id === "registro_email" && !emailValido(value)) {
     alert("Email no es válido");
     return false;
-  }
-  else if (id === "registro_password" && !contrasenaValida(value)) {
+  } else if (id === "registro_password" && !contrasenaValida(value)) {
     alert("Password no cumple los requisitos");
     return false;
-  }
-  else if(id ==="registro_comentarios" && !comentarioValido(value)){
+  } else if (
+    id === "registro_comentarios" &&
+    (!comentarioValido(value) || value.trim() === "")
+  ) {
+    if (value.trim() === "") {
+      alert("El comentario es obligatorio");
+      return false;
+    }
     alert("El comentario no debe exceder los 50 caracteres");
     return false;
   }
@@ -30,15 +33,18 @@ function emailValido(email) {
 }
 
 function contrasenaValida(contrasena) {
-  if (/[a-z]/.test(contrasena) && /[A-Z]/.test(contrasena) && /\d/.test(contrasena)) {
+  if (
+    /[a-z]/.test(contrasena) &&
+    /[A-Z]/.test(contrasena) &&
+    /\d/.test(contrasena)
+  ) {
     return true;
   }
   return false;
 }
 
-function comentarioValido(comentario){
-  console.log(comentario.length)
-  return comentario.length < 50
+function comentarioValido(comentario) {
+  return comentario.length < 50;
 }
 
 function validarFormulario() {
@@ -53,7 +59,7 @@ function validarFormulario() {
   );
   for (let i = 0; i < inputs.length; i++) {
     if (!validateField(inputs[i].id, inputs[i].value)) {
-      return false; 
+      return false;
     }
   }
   return true;
